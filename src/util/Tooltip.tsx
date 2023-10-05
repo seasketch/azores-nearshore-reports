8d1a92fa409e60ea8509dde7084bf0821795268c
Abby Vath Meyer <avmey@ucsb.edu>
2023-10-05 22:29:28 +0000

    nit

--- src/util/Tooltip.tsx
+++ src/util/Tooltip.tsx
@@ -73,12 +73,6 @@ export const Tooltip = ({
             offset: [horizontal, vertical],
           },
         },
-        {
-          name: "arrow",
-          options: {
-            padding: 5,
-          },
-        },
       ],
     }
   );
@@ -109,7 +103,6 @@ export const Tooltip = ({
           <TooltipItem>{text}</TooltipItem>
         </TooltipContainer>
       </div>
-      <div className="arrow" style={styles.arrow} {...attributes.arrow}></div>
     </React.StrictMode>
   );
 };
